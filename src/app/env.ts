aaaa934cccee4d2854b58b56e583b62f49c02995
Dylan Barduzzi <dlbarduzzi@gmail.com>
2025-03-18 14:44:12 -0500

    Config env variables

---
+++ src/app/env.ts
@@ -0,0 +1,23 @@
+import { z } from "zod"
+import { config } from "dotenv"
+import { expand } from "dotenv-expand"
+
+expand(config())
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["test", "development", "production"]),
+  APP_URL: z.string().url(),
+  APP_PORT: z.coerce.number(),
+})
+
+// eslint-disable-next-line no-process-env
+const envParsed = envSchema.safeParse(process.env)
+
+if (!envParsed.success) {
+  console.error("❌ Invalid server environment variables ❌")
+  console.error(JSON.stringify(envParsed.error.flatten().fieldErrors, null, 2))
+  // eslint-disable-next-line no-process-exit
+  process.exit(1)
+}
+
+export const env = envParsed.data
--- src/serve.ts
+++ src/serve.ts
@@ -1,6 +1,8 @@
 import { serve } from "@hono/node-server"
 import { Hono } from "hono"
 
+import { env } from "./app/env"
+
 const app = new Hono()
 
 app.get("/", c => {
@@ -10,7 +12,7 @@ app.get("/", c => {
 serve(
   {
     fetch: app.fetch,
-    port: 3000,
+    port: env.APP_PORT,
   },
   info => {
     console.log(`Server is running on http://localhost:${info.port}`)
